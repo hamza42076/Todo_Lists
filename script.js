6c7dafdb0bc232a73dbfcea0f20c75cdd16fab8f
hamza42076 <164366638+hamza42076@users.noreply.github.com>
2025-05-11 00:10:20 +0500

    first commit

--- script.js
+++ script.js
@@ -1,63 +1,77 @@
 let todoBox = document.querySelector(".todoBox");
+let todoArray = [];
+let editIndex = null; // For edit tracking
 
-let todoArray =  [];
-
-function toGetlocalStorage(){
-    let data = JSON.parse(window.localStorage.getItem("todoArray"))
-    if(data){
-        todoArray = data
+function toGetlocalStorage() {
+    let data = JSON.parse(window.localStorage.getItem("todoArray"));
+    if (data) {
+        todoArray = data;
     }
     console.log(data);
-    
 }
-toGetlocalStorage()
+toGetlocalStorage();
 
-function toStorelocalStorage(){
-    window.localStorage.setItem("todoArray",JSON.stringify(todoArray))
+function toStorelocalStorage() {
+    window.localStorage.setItem("todoArray", JSON.stringify(todoArray));
 }
 toStorelocalStorage();
-
-
 
 function randorbox() {
     todoBox.innerHTML = "";
 
     for (let i = 0; i < todoArray.length; i++) {
-
         let todoDiv = document.createElement("div");
         let todoSpan = document.createElement("span");
         todoSpan.style.padding = "10px";
 
-        let todoButton = document.createElement("button");        
+        let todoButton = document.createElement("button");
         todoButton.innerText = "delete";
-        todoButton.addEventListener("click",function(e){
+        todoButton.style.marginLeft = "5px";
+        todoButton.addEventListener("click", function (e) {
             todoButton.id = i;
             let todobtnId = e.target.id;
-            todoArray.splice(todobtnId,1);
+            todoArray.splice(todobtnId, 1);
             toStorelocalStorage();
             randorbox();
-            
-        });    
-    todoDiv.className = "todo";
-    todoDiv.innerText = todoArray[i];
-    
-    todoDiv.appendChild(todoSpan);
-    todoDiv.appendChild(todoButton)
-    todoBox.appendChild(todoDiv);
-    }
-    
-}
-randorbox()
+        });
 
-function handleAdd(){
-    
-    let todo_input = document.querySelector("#todo-input").value;
-    if(todo_input == ""){
-        alert("enter todo");
-        return
+        // --- EDIT button ---
+        let editButton = document.createElement("button");
+        editButton.innerText = "edit";
+        editButton.style.marginLeft = "5px";
+        editButton.addEventListener("click", function () {
+            document.querySelector("#todo-input").value = todoArray[i];
+            editIndex = i;
+            document.querySelector("#add-btn").innerText = "Update";
+        });
+
+        todoDiv.className = "todo";
+        todoDiv.innerText = todoArray[i];
+
+        todoDiv.appendChild(todoSpan);
+        todoDiv.appendChild(editButton);
+        todoDiv.appendChild(todoButton);
+        todoBox.appendChild(todoDiv);
     }
-    todoArray.push(todo_input);
+}
+randorbox();
+
+function handleAdd() {
+    let todo_input = document.querySelector("#todo-input").value;
+    if (todo_input == "") {
+        alert("enter todo");
+        return;
+    }
+
+    if (editIndex !== null) {
+        todoArray[editIndex] = todo_input;
+        editIndex = null;
+        document.querySelector("#add-btn").innerText = "Add";
+    } else {
+        todoArray.push(todo_input);
+    }
+
     toStorelocalStorage();
     randorbox();
-    
+    document.querySelector("#todo-input").value = ""; // Clear input
 }
